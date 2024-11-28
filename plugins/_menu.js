11d585ead1a53d045ba575527af2362b417d732e
King Phantom <kingphantom488@gmail.com>
2024-11-28 21:32:38 +0200

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -50,15 +50,16 @@ astro_patch.smd({
     menuContent += `*Uptime*: ${runtime(process.uptime())}\n`;
     menuContent += `*Date*: ${currentDate}\n`;
     menuContent += `*THANKS TO*: BARAKA BEGA\n`;
-    menuContent += `👋😄 ${greeting}\n\n`;
+    menuContent += `👋 😄 ${greeting}\n\n`;
 
     menuContent += readmore + "\n\n"; // Adding readmore before commands
 
     // List commands by category
     for (const category in commandCategories) {
-      menuContent += `${tiny(category)}═════════⊷❍\n`; // Decorative line with category inside
+      menuContent += `${tiny(category)} ════════⊷❍\n`; // Decorative line with category inside
       commandCategories[category].forEach(cmd => {
-        menuContent += `┊❍${fancytext(cmd, 1)}\n`; // Changed emoji here
+        menuContent += `┊❍. ${fancytext(cmd, 1)}\n`; // Changed emoji here
+      });
       menuContent += `CONTINUE\n${readmore}\n\n`; // Adding readmore with 👇 after each category
     }
 
